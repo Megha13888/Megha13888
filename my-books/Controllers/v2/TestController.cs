1d822e4e92c9c0e4f67508abf92312ff43ffdcdf
Megha Patel <mpatel@gasleaksensors.com>
2022-06-15 12:38:37 +0530

    getTestDatav2

--- my-books/Controllers/v2/TestController.cs
+++ my-books/Controllers/v2/TestController.cs
@@ -43,5 +43,11 @@ namespace my_books.Controllers.v2
         {
             return Ok("Development API v1");
         }
+
+        [HttpGet("getTestDatav2")]
+        public IActionResult GetTestDatav1()
+        {
+            return Ok("Development API v2");
+        }
     }
 }
